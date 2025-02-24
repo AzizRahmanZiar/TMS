2d857f9600e35d02573e65abf8b4329b92b7cbb2
AzizRahmanZiar <azizziar1401@gmail.com>
2025-02-25 00:45:16 +0430

    error sovled

--- laravel_inertia_react/resources/js/Components/Layout.jsx
+++ laravel_inertia_react/resources/js/Components/Layout.jsx
@@ -1,40 +1,11 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Sidebar from "./Sidebar";
-// import Cloths from "./Cloths";
-// import Uniform from "./Uniform";
-// import Sadrai from "./Sadrai";
-// import Kortai from "./Kortai";
-// import Home from "./Dashboard";
-
-// const App = () => (
-//     <Router>
-//         <div className="flex">
-//             <Sidebar />
-//             <div className="overflow-x-auto bg-white py-5 w-[100%]">
-//                 <Routes>
-//                     <Route path="/dashboard" element={<Home />} />
-//                     <Route path="/cloths" element={<Cloths />} />
-//                     <Route path="/uniform" element={<Uniform />} />
-//                     <Route path="/kortai" element={<Kortai />} />
-//                     <Route path="/sadrai" element={<Sadrai />} />
-//                 </Routes>
-//             </div>
-//         </div>
-//     </Router>
-// );
-
-// export default App;
-
-// src/Layout.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./Sidebar";
 import Cloths from "../Pages/Cloths";
 import Uniform from "../Pages/Uniform";
 import Sadrai from "../Pages/Sadrai";
 import Kortai from "../Pages/Kortai";
 import Dashboard from "../Pages/Dashboard";
+import Sidebar from "./Sidebar";
 
 const Layout = () => (
     <Router>
