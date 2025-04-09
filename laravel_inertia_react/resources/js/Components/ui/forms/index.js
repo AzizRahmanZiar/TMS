4339dd4f1e64ef1bee23c82a33c537b4a63cee0d
AzizRahmanZiar <azizziar1401@gmail.com>
2025-04-09 23:04:51 +0430

    extra files deleted

--- laravel_inertia_react/resources/js/Components/ui/forms/index.js
+++
@@ -1,7 +0,0 @@
-import Form from "./Form";
-import FormField from "./FormField";
-import FormGroup from "./FormGroup";
-import FormActions from "./FormActions";
-
-export { Form, FormField, FormGroup, FormActions };
-export default Form;
